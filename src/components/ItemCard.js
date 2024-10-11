403efc667c35576caeb4531849450ea63de468e1
Isfaqul <isfaq.xperia@gmail.com>
2024-10-11 21:26:46 +0530

    Refactor : Create Item Card component

---
+++ src/components/ItemCard.js
@@ -0,0 +1,30 @@
+import "./ItemCard.scss";
+
+export default function ItemCard(item) {
+  const card = document.createElement("article");
+  card.classList.add("item-card");
+
+  const itemImage = document.createElement("img");
+  itemImage.classList.add("item-image");
+  itemImage.src = item.src;
+  itemImage.alt = item.title;
+
+  card.append(itemImage);
+
+  const title = document.createElement("h2");
+  title.classList.add("item-title");
+  title.innerText = item.title;
+  card.append(title);
+
+  const info = document.createElement("p");
+  info.classList.add("item-info");
+  info.innerText = item.info;
+  card.append(info);
+
+  const price = document.createElement("p");
+  price.classList.add("item-price");
+  price.innerText = "$ " + item.price.toFixed(2);
+  card.append(price);
+
+  return card;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import "./styles.scss";
+import ItemCard from "./components/ItemCard";
+import data from "./data.json";
 
-console.log("Hello world!");
+const itemContainer = document.querySelector(".items-container");
+
+function displayItems() {
+  data.forEach((item) => {
+    const newItem = ItemCard(item);
+    itemContainer.append(newItem);
+  });
+}
+
+displayItems();
